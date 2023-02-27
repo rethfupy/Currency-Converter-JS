c1d16282988c39ad5bbd4f9c1389ec4a60490d25
rethfupy <solamdev@outlook.com>
2023-02-27 15:33:25 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const valueNode = document.querySelector('.value-input');
+const baseCurrencyNode = document.querySelector('.baseCurrency');
+const convertToCurrencyNode = document.querySelector('.convertToCurrency');
+const output = document.querySelector('.output');
+
+valueNode.addEventListener('input', calculate, false);
+baseCurrencyNode.addEventListener('change', calculate, false);
+convertToCurrencyNode.addEventListener('change', calculate, false);
+
+function calculate(){
+    const value = Number(valueNode.value);
+    const baseCurrency = (document.querySelector('.baseCurrency')).value;
+    const convertToCurrency = (document.querySelector('.convertToCurrency')).value;
+
+    if(baseCurrency == convertToCurrency){
+        alert("Please pick a different currency to convert");
+        return false;
+    }
+
+    const API = "https://api.exchangerate.host/convert?from=" + baseCurrency + "&to=" + convertToCurrency;
+    
+    fetch(API)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let request = data;
+            let rate = request.result;
+            let result = (value * rate).toFixed(2);
+            output.innerHTML = result;
+        });
+}
